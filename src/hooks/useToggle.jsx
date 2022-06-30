050e7dabbebaed4878b53a2009f708e371b8bc12
MiladSedhom <winterqq1@gmail.com>
2022-06-30 20:59:10 +0200

    more useToggle bugfixes

--- src/hooks/useToggle.jsx
+++ src/hooks/useToggle.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export function useToggle(_default) {
 
     const [status,setStatus] = useState( _default || false )
-    const toggleStatus = ()=> setStatus(prevState => !prevState)
+    const toggleStatus = ()=> {setStatus(prevState => !prevState)}
 
     return [status,toggleStatus]
 }
