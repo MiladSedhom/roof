fb311c11f90c5ebac958f7bb733c12bdc1219e8b
MiladSedhom <winterqq1@gmail.com>
2022-09-18 14:43:47 +0200

    add contextMenu to useCLickOutside

--- src/hooks/useClickOutside.jsx
+++ src/hooks/useClickOutside.jsx
@@ -3,14 +3,17 @@ import { useEffect } from "react"
 export const useClickOutside = (ref, callback) => {
 	const handleClick = e => {
 		if (!ref?.current?.contains(e.target) && callback) {
+			e.preventDefault()
 			callback(e)
 			e.stopPropagation()
 		}
 	}
 	useEffect(() => {
 		document.addEventListener("click", handleClick, true)
+		document.addEventListener("contextmenu", handleClick, true)
 		return () => {
 			document.removeEventListener("click", handleClick, true)
+			document.removeEventListener("contextmenu", handleClick, true)
 		}
 	}, [])
 }
