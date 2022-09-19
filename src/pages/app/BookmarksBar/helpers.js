12057d4bb5b2df58273e022c60c2cf2a8d4be7a7
MiladSedhom <winterqq1@gmail.com>
2022-09-20 00:06:26 +0200

    refactor: isValidURL()

--- src/pages/app/BookmarksBar/helpers.js
+++ src/pages/app/BookmarksBar/helpers.js
@@ -25,8 +25,13 @@ export function getListPosition(buttonPosition, isNested, listWidth) {
 }
 
 export function isValidURL(string) {
-	var res = string.match(
-		/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-	)
-	return res !== null
+	let url
+
+	try {
+		url = new URL(string)
+	} catch (_) {
+		return false
+	}
+
+	return true
 }
