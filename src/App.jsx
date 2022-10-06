b9b2f3355e6786d78314463b427ee71a44c1bd14
MiladSedhom <winterqq1@gmail.com>
2022-10-06 20:33:23 +0200

    refactor: add AppPage
    instead of using the Actual App.jsx file
    now the theme is actually global

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,18 @@
-import styled, { createGlobalStyle } from "styled-components"
-import BookmarksBar from "./pages/app/BookmarksBar/BookmarksBar"
-import SearchBar from "./pages/app/SearchBar/SearchBar"
-import { useToggle } from "./hooks/useToggle"
-import SettingsModal from "./pages/app/SettingsModal/SettingsModal"
-import { ThemeContext, theme } from "./contexts/ThemeContext"
-import { useEffect } from "react"
-import { BookmarksProvider } from "./stores/useBookmarksStore"
-import { ShortcutsProvider } from "./stores/useShortcutStore"
-
+import { ThemeProvider } from "./stores/useThemeStore"
+import AppPage from "./pages/app/AppPage"
 let renderCount = 1
 
 function App() {
 	console.log(renderCount)
 	renderCount++
 
-	const [isSettings, toggleIsSettings] = useToggle(false)
-
 	return (
 		<div className="App">
-			<GlobalStyle />
-			<BookmarksProvider>
-				<ThemeContext.Provider value={theme}>
-					<StyledApp>
-						<BookmarksBar toggleIsSettings={toggleIsSettings} />
-
-						<Container>
-							<ShortcutsProvider>
-								<SearchBarLogoContainer>
-									<Logo>Roof</Logo>
-									<SearchBar />
-								</SearchBarLogoContainer>
-							</ShortcutsProvider>
-						</Container>
-					</StyledApp>
-				</ThemeContext.Provider>
-			</BookmarksProvider>
+			<ThemeProvider>
+				<AppPage />
+			</ThemeProvider>
 		</div>
 	)
 }
 
 export default App
-
-const StyledApp = styled.div`
-	background: #b5a484; /*#888888; */
-	height: 100vh;
-	position: relative;
-`
-
-const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: calc(100vh - 2.5rem);
-`
-
-const SearchBarLogoContainer = styled.div`
-	display: flex;
-	flex-flow: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	margin: 0 0 10rem 0;
-`
-const Logo = styled.p`
-	font-family: Fascinate;
-	font-size: 3rem;
-	margin: 1rem;
-	color: ${theme.containersColor};
-`
-
-const GlobalStyle = createGlobalStyle`
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-/* width */
-::-webkit-scrollbar {
-  width: 10px;
-}
-/* Track */
-::-webkit-scrollbar-track {
-  background: #484848;
-}
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #2f2f2f;
-}
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #212020;
-}
-`
---
+++ src/pages/app/AppPage.jsx
@@ -0,0 +1,108 @@
+import styled, { createGlobalStyle } from "styled-components"
+import { useStack } from "../../hooks/useStack"
+import { useToggle } from "../../hooks/useToggle"
+import { useThemeDispatch, useThemeStore } from "../../stores/useThemeStore"
+import BookmarksBar from "./BookmarksBar/BookmarksBar"
+import SearchBar from "./SearchBar/SearchBar"
+import SettingsModal from "./SettingsModal/SettingsModal"
+import { BookmarksProvider } from "../../stores/useBookmarksStore"
+import { ShortcutsProvider } from "../../stores/useShortcutStore"
+
+let renderCount = 1
+
+export default function AppPage() {
+	console.log(renderCount)
+	renderCount++
+
+	const [isSettings, toggleIsSettings] = useToggle(false)
+	const undoStack = useStack([])
+
+	const themeDispatch = useThemeDispatch()
+	return (
+		<div>
+			<GlobalStyle />
+			<StyledApp>
+				<BookmarksProvider>
+					<BookmarksBar toggleIsSettings={toggleIsSettings} />
+				</BookmarksProvider>
+
+				<ShortcutsProvider>
+					{isSettings && <SettingsModal toggleIsSettings={toggleIsSettings} undoStack={undoStack} />}
+					<Container>
+						<SearchBarLogoContainer>
+							<Logo>Roof</Logo>
+							<SearchBar />
+						</SearchBarLogoContainer>
+					</Container>
+				</ShortcutsProvider>
+			</StyledApp>
+		</div>
+	)
+}
+
+const StyledApp = styled.div`
+	background-color: ${props => {
+		const theme = useThemeStore()
+		return theme.backgroundColor
+	}};
+	height: 100vh;
+	position: relative;
+`
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: calc(100vh - 2.5rem);
+`
+
+const SearchBarLogoContainer = styled.div`
+	display: flex;
+	flex-flow: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	margin: 0 0 10rem 0;
+`
+const Logo = styled.p`
+	font-family: Fascinate;
+	font-size: 3rem;
+	margin: 1rem;
+	color: ${props => {
+		const theme = useThemeStore()
+		return theme.containersColor
+	}};
+`
+
+const GlobalStyle = createGlobalStyle`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #484848;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #2f2f2f;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #212020;
+}
+`
