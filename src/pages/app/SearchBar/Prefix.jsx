fa9e4b209c4b8eec177d0934d240f698babb41c9
MiladSedhom <MiladSedhom@gmail.com>
2024-07-04 13:53:14 +0300

    tiny style tweak

--- src/pages/app/SearchBar/Prefix.jsx
+++ src/pages/app/SearchBar/Prefix.jsx
@@ -1,21 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export default function Prefix(props) {
-	const { name, textColor, backgroundColor } = props;
+	const { name, textColor, backgroundColor } = props
 
 	return (
 		<StyledSpan backgroundColor={backgroundColor} textColor={textColor}>
 			{name}
 		</StyledSpan>
-	);
+	)
 }
 
 const StyledSpan = styled.span`
 	align-self: center;
 	flex-shrink: 0;
 	padding: 0.25em 0.5em;
-	margin: 0 0 0 1em;
+	margin: 0 0 0 0.5em;
 	border-radius: 10rem;
-	background-color: ${(props) => props.backgroundColor || "darkgrey"};
-	color: ${(props) => props.textColor || "white"};
-`;
+	background-color: ${props => props.backgroundColor || 'darkgrey'};
+	color: ${props => props.textColor || 'white'};
+`
